f5627e603841ab6ff22614e985c95c7506961dba
NiK <nikakarumidze@gmail.com>
2022-12-10 16:15:09 +0400

    Remark: technology page instead of destination

--- src/pages/Crew/MemberInfo.jsx
+++ src/pages/Crew/MemberInfo.jsx
@@ -16,7 +16,11 @@ const MemberInfo = (props) => {
         container
         alignItems='start'
         justifyContent='space-around'
-        direction={{ xs: 'column !important', sm:'column-reverse !important',md: 'row !important' }}
+        direction={{
+          xs: 'column !important',
+          sm: 'column-reverse !important',
+          md: 'row !important',
+        }}
         sx={{ width: 'auto' }}
       >
         <Grid
@@ -26,15 +30,15 @@ const MemberInfo = (props) => {
           md={6}
           sx={{ mt: { xs: 0, md: 10 } }}
           alignItems='center'
-          justifyContent={{xs:'center',md:'start'}}
-          direction={{xs:'row', sm: 'column-reverse', md:'row'}}
+          justifyContent={{ xs: 'center', md: 'start' }}
+          direction={{ xs: 'row', sm: 'column-reverse', md: 'row' }}
         >
           <Grid
             container
             item
             alignItems={{ xs: 'center', md: 'start' }}
             direction='column'
-            sx={{mb:5}}
+            sx={{ mb: 5 }}
           >
             <Typography variant='h4' sx={{ mt: 5 }}>
               {props.position.toUpperCase()}
@@ -55,7 +59,7 @@ const MemberInfo = (props) => {
           xs={12}
           md={6}
           justifyContent='center'
-          sx={{ mt: { xs: 1, md: 0 }, mb: {xs:0,sm:2,md:0} }}
+          sx={{ mt: { xs: 1, md: 0 }, mb: { xs: 0, sm: 2, md: 0 } }}
         >
           <CardMedia
             component='img'
